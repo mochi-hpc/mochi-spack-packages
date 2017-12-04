3758a042b5af39670bce3007d4ac2d7817fa5032
Matthieu Dorier <mdorier@anl.gov>
2017-12-04 12:34:54 +0100

    added thallium package

---
+++ packages/thallium/package.py
@@ -0,0 +1,12 @@
+from spack import *
+
+class Thallium(CMakePackage):
+    """Thallium is a C++14 library wrapping Margo, Mercury, and Argobots and providing an object-oriented way to use these libraries."""
+    homepage = "https://xgitlab.cels.anl.gov/sds/thallium"
+    url = "https://xgitlab.cels.anl.gov/sds/thallium"
+
+    version('master', git='https://xgitlab.cels.anl.gov/sds/thallium.git')
+
+#depends_on('mercury')
+#depends_on('argobots')
+    depends_on('margo')
