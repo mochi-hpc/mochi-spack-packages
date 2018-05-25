30c27b0eb80823a19f22e69586136e41b59be744
Matthieu Dorier <mdorier@anl.gov>
2018-05-25 16:03:06 +0200

    modified flamestore

--- packages/flamestore/package.py
+++ packages/flamestore/package.py
@@ -14,11 +14,8 @@ class Flamestore(PythonPackage):
     url      = "https://xgitlab.cels.anl.gov/sds/flame-store.git"
 
     version('master',  git="https://xgitlab.cels.anl.gov/sds/flame-store.git")
-    version('mochi', git="https://xgitlab.cels.anl.gov/sds/flame-store.git",
-            branch='dev-mochi-backend', preferred=True)
 
     depends_on('py-margo')
-    depends_on('py-sdskv')
     depends_on('py-bake+numpy')
     depends_on('py-h5py')
     depends_on('py-keras')
