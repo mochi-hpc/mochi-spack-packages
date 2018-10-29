3ce23c39ff0d9c6b2071e6d68bd12c807b28b98b
Matthieu Dorier <mdorier@anl.gov>
2018-10-29 12:31:54 -0500

    updated thallium package to match version 0.3

--- packages/thallium/package.py
+++ packages/thallium/package.py
@@ -1,13 +1,17 @@
 from spack import *
 
 class Thallium(CMakePackage):
-    """Thallium is a C++14 library wrapping Margo, Mercury, and Argobots and providing an object-oriented way to use these libraries."""
+    """Thallium is a C++14 library wrapping Margo, Mercury,
+    and Argobots and providing an object-oriented way to use these libraries."""
+
     homepage = "https://xgitlab.cels.anl.gov/sds/thallium"
     url = "https://xgitlab.cels.anl.gov/sds/thallium"
+    git = "https://xgitlab.cels.anl.gov/sds/thallium.git"
 
-    version('master', git='https://xgitlab.cels.anl.gov/sds/thallium.git')
+    version('develop', branch='master')
+    version('0.3', tag='v0.3', preferred=True)
 
-    depends_on('margo')
+    depends_on('margo@0.4:')
     # thallium relies on std::decay_t
     conflicts('%gcc@:4.9.0');
 
