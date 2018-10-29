699d15c08663db52869d9ad2e4742c77d7f60c6a
Rob Latham <robl@mcs.anl.gov>
2018-10-29 11:15:14 -0500

    preparing for 1.0 release

--- packages/mercury/package.py
+++ packages/mercury/package.py
@@ -30,10 +30,13 @@ class Mercury(CMakePackage):
 
     homepage = "https://mercury-hpc.github.io/"
     url = "https://mercury-hpc.github.io/"
+    git = "https://github.com/mercury-hpc/mercury.git"
 
 
-    version('master', git='https://github.com/mercury-hpc/mercury.git',
-            submodules=True)
+    version('develop', branch='master', submodules=True)
+    version ('master', branch='master', submodules=True)
+    version('1.0.0rc1', tag='v1.0.0rc1', submodules=True, preferred=True)
+    version('0.9.0', tag='v0.9.0', submodules=True)
 
     variant('cci', default=False, description="Use CCI for network transport")
     variant('bmi', default=False, description="Use BMI for network transport")
