e86ee5b9cf613f7d1d7504b2eabd2aabe33439c5
Matthieu Dorier <mdorier@anl.gov>
2018-10-29 13:06:54 -0500

    version 0.1 of bake package

--- packages/bake/package.py
+++ packages/bake/package.py
@@ -2,17 +2,19 @@ from spack import *
 
 
 class Bake(AutotoolsPackage):
-    """FIXME: Put a proper description of your package here."""
+    """Margo-enabled wrapper for Pmem, enabling RDMA to Pmem targets."""
 
     homepage = "https://xgitlab.cels.anl.gov/sds/bake"
     url      = "https://xgitlab.cels.anl.gov/sds/bake"
+    git      = 'https://xgitlab.cels.anl.gov/sds/bake.git'
 
-    version('master', git='https://xgitlab.cels.anl.gov/sds/bake.git')
+    version('develop', branch='master')
+    version('0.1', tag='v0.1')
 
     depends_on('autoconf@2.65:', type=("build"))
     depends_on('automake@1.13.4:', type=("build"))
     depends_on('libtool', type=("build"))
-    depends_on('margo')
-    depends_on('remi')
+    depends_on('margo@0.4:')
+    depends_on('remi@0.1:')
     depends_on('libuuid')
     depends_on('pmem')
