b4ed6fbb86b178e21c4cbc7199ce1af1577beeaa
Rob Latham <robl@mcs.anl.gov>
2019-03-05 10:43:52 -0600

    Update libpmemobj-cpp package
    There have been a few new releases.
    
    Also fixed the installation so pkg-config file installed along with
    everything else

--- packages/libpmemobj-cpp/package.py
+++ packages/libpmemobj-cpp/package.py
@@ -5,12 +5,20 @@ class LibpmemobjCpp(CMakePackage):
     """C++ bindings for libpmemobj (https://github.com/pmem/pmdk)"""
 
     homepage = "https://github.com/pmem/libpmemobj-cpp"
-    url      = "https://github.com/pmem/libpmemobj-cpp.git"
+    url      = "https://github.com/pmem/libpmemobj-cpp"
+    git      =  "https://github.com/pmem/libpmemobj-cpp.git"
 
     version('master', git='https://github.com/pmem/libpmemobj-cpp.git')
+    version('1.5', tag='1.5')
+    version('1.5.1', tag='1.5.1')
+
+    # libpmemobj seems to only understand 'Debug' and 'Release'
+    variant('build_type', default='Release',
+            description='CMake build type',
+            values=('Debug', 'Release'))
 
 
-    depends_on('pmdk@1.4')
+    depends_on('pmdk@1.4:')
 
     def cmake_args(self):
         # Add arguments other than
