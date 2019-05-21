421cf5ca40f17754ad97c424c7e0bb19305d7e33
Matthieu Dorier <mdorier@anl.gov>
2019-05-21 12:05:51 -0500

    added new version of thallium

--- packages/thallium/package.py
+++ packages/thallium/package.py
@@ -9,6 +9,7 @@ class Thallium(CMakePackage):
     git = "https://xgitlab.cels.anl.gov/sds/thallium.git"
 
     version('develop', branch='master')
+    version('0.3.4', tag='v0.3.4')
     version('0.3.3', tag='v0.3.3')
     version('0.3.2', tag='v0.3.2')
     version('0.3.1', tag='v0.3.1')
