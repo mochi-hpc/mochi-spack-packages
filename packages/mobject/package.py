aa1186c9f282017787a3f75c312a1d1df01394aa
Rob Latham <robl@mcs.anl.gov>
2020-01-29 11:06:03 -0600

    small typo fix

--- packages/mobject/package.py
+++ packages/mobject/package.py
@@ -52,7 +52,7 @@ class Mobject(AutotoolsPackage):
     depends_on('sdskeyval@0.1:')
     depends_on('bake@0.1:')
     depends_on('bake@0.3:0.3.6', when='@0.3:')
-    depends_on('bake@0.4:', when '@0.4.2:')
+    depends_on('bake@0.4:', when='@0.4.2:')
 
     patch('0001-crude-timing-information.patch', when="+timing")
 
