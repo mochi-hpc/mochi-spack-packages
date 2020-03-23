d85534a0967fc646dfaa2d6f947c57068eb4b012
Matthieu Dorier <mdorier@anl.gov>
2020-03-23 13:50:11 +0000

    added master version of thallium

--- packages/mochi-thallium/package.py
+++ packages/mochi-thallium/package.py
@@ -8,6 +8,7 @@ class MochiThallium(CMakePackage):
     url = "https://xgitlab.cels.anl.gov/sds/thallium"
     git = "https://xgitlab.cels.anl.gov/sds/thallium.git"
 
+    version('master', branch='master')
     version('develop', branch='master')
     version('0.5.2', tag='v0.5.2')
     version('0.5.1', tag='v0.5.1')
