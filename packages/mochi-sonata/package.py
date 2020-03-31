6fe34d9e5657dfa387be5ff7aa1b1b08cb85ba93
Matthieu Dorier <mdorier@anl.gov>
2020-03-31 19:25:47 +0100

    Added package for Sonata

---
+++ packages/mochi-sonata/package.py
@@ -0,0 +1,24 @@
+from spack import *
+
+class MochiSonata(CMakePackage):
+    """Sonata is a Mochi-based document store that uses UnQLite as a backend."""
+
+    homepage = "https://xgitlab.cels.anl.gov/sds/sonata"
+    url = "https://xgitlab.cels.anl.gov/sds/sonata"
+    git = "https://xgitlab.cels.anl.gov/sds/sonata.git"
+
+    version('master', branch='master', preferred=True)
+    version('develop', branch='master')
+    version('0.1', tag='v0.1')
+
+    depends_on('mochi-thallium@develop', when='@develop')
+    depends_on('mochi-thallium')
+    depends_on('unqlite')
+    depends_on('tclap')
+    depends_on('jsoncpp')
+    depends_on('spdlog')
+    depends_on('cmake')
+
+    def cmake_args(self):
+        args = ["-DBUILD_SHARED_LIBS:BOOL=ON" ]
+        return args
