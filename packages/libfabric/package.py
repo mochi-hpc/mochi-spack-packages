0196544bd1dc9416046c6f42d7511fa4e617c1b1
Rob Latham <robl@mcs.anl.gov>
2020-04-07 11:16:01 -0400

    updates for 1.10.rc1

--- packages/libfabric/package.py
+++ packages/libfabric/package.py
@@ -10,3 +10,16 @@ class Libfabric(Libfabric):
     version('1.9.1',
     sha256='c305c6035c992523e08c7591a6a3707225ba3e72de40443eaed837a10df6771a',
         url='https://github.com/ofiwg/libfabric/releases/download/v1.9.1/libfabric-1.9.1.tar.bz2', preferred=True)
+    version('1.10.rc1', tag='v1.10.0rc1')
+    depends_on('m4', when='@1.10.rc1', type=('build'))
+    depends_on('autoconf', when='@1.10.rc1', type=('build'))
+    depends_on('automake', when='@1.10.rc1', type=('build'))
+    depends_on('libtool', when='@1.10.rc1', type=('build'))
+
+    @when('@1.10.rc1')
+    def autoreconf(self, spec, prefix):
+        bash = which('bash')
+        bash('./autogen.sh')
+        with working_dir('fabtests'):
+            bash('./autogen.sh')
+
