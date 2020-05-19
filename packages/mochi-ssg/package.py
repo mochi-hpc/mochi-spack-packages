a9bf4b7d3d5e51e15af15dd4c68ed6021d91824c
Rob Latham <robl@mcs.anl.gov>
2020-05-19 15:46:32 -0500

    extra error checking for ssg

--- packages/mochi-ssg/package.py
+++ packages/mochi-ssg/package.py
@@ -21,6 +21,7 @@ class MochiSsg(AutotoolsPackage):
 
     patch('0001-additional-error-reporting.patch', when='@0.2')
     patch('0001-Add-an-explicit-check-for-pmix-libs.patch', when='@0.4.1')
+    patch('0001-Improve-deserialize-error-reporting.patch', when='@0.4.1')
 
     variant('mpi', default=True, description='Build with MPI support')
     variant('pmix', default=False, description='Build with PMIx support')
