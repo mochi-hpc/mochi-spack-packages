82416a5c6e009b6834952a29c39246448802e94e
Rob Latham <robl@mcs.anl.gov>
2020-05-22 13:06:55 -0500

    straighten out libev/libevent
    Need to pick up https://github.com/spack/spack/pull/16530 until
    available in released version.

---
+++ packages/prrte/package.py
@@ -0,0 +1,24 @@
+from spack.pkg.builtin.prrte import Prrte
+
+class Prrte(Prrte):
+    depends_on('libev')
+
+    def configure_args(self):
+        # spack-0.14.2 prrte configure asks for libev (--with-libev) but passes
+        # libevent.  we have to replicate the configure, not extend it, until
+        # released version of spack has fix.
+        spec = self.spec
+        config_args = [
+            '--enable-shared',
+            '--enable-static'
+        ]
+
+        # libevent
+        config_args.append(
+            '--with-libevent={0}'.format(spec['libevent'].prefix))
+        # hwloc
+        config_args.append('--with-hwloc={0}'.format(spec['hwloc'].prefix))
+        # pmix
+        config_args.append('--with-pmix={0}'.format(spec['pmix'].prefix))
+
+        return config_args
