95bfaf0ed57e45daa87b6ff193646541fb29354f
Rob Latham <robl@mcs.anl.gov>
2020-08-18 10:07:37 -0500

    libfabric-1.11.0 is out

--- packages/libfabric/package.py
+++ packages/libfabric/package.py
@@ -9,7 +9,12 @@ class Libfabric(Libfabric):
 
     version('1.10.1',
     sha256='889fa8c99eed1ff2a5fd6faf6d5222f2cf38476b24f3b764f2cbb5900fee8284',
-        url='https://github.com/ofiwg/libfabric/releases/download/v1.10.1/libfabric-1.10.1.tar.bz2', preferred=True)
+        url='https://github.com/ofiwg/libfabric/releases/download/v1.10.1/libfabric-1.10.1.tar.bz2')
+    version('1.11.0',
+            sha256='9938abf628e7ea8dcf60a94a4b62d499fbc0dbc6733478b6db2e6a373c80d58f',
+        url='https://github.com/ofiwg/libfabric/releases/download/v1.11.0/libfabric-1.11.0.tar.bz2', preferred=True)
+
+#            
 #    depends_on('m4', when='@1.10.1', type=('build'))
 #    depends_on('autoconf', when='@1.10.1', type=('build'))
 #    depends_on('automake', when='@1.10.1', type=('build'))
