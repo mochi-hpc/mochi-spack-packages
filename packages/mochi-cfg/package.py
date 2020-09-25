de2db2cbd6d018cb5aa1b9a5ef7ffc93e4f68761
Phil Carns <carns@mcs.anl.gov>
2020-09-25 16:41:49 -0400

    tagged mochi-cfg release

--- packages/mochi-cfg/package.py
+++ packages/mochi-cfg/package.py
@@ -31,6 +31,7 @@ class MochiCfg(AutotoolsPackage):
     homepage = 'https://xgitlab.cels.anl.gov/sds/mochi-cfg'
     git = 'https://xgitlab.cels.anl.gov/sds/mochi-cfg.git'
 
+    version('0.1', tag='v0.1', preferred=True)
     version('develop', branch='master')
     version('master', branch='master')
 
