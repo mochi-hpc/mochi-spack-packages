0de693c598b844f23ef813979d0ed8b3f3d3d1d1
Phil Carns <carns@mcs.anl.gov>
2020-10-13 08:24:04 -0400

    new spack version of mochi-cfg

--- packages/mochi-cfg/package.py
+++ packages/mochi-cfg/package.py
@@ -31,7 +31,8 @@ class MochiCfg(AutotoolsPackage):
     homepage = 'https://xgitlab.cels.anl.gov/sds/mochi-cfg'
     git = 'https://xgitlab.cels.anl.gov/sds/mochi-cfg.git'
 
-    version('0.1', tag='v0.1', preferred=True)
+    version('0.2', tag='v0.2')
+    version('0.1', tag='v0.1')
     version('develop', branch='master')
     version('master', branch='master')
 
