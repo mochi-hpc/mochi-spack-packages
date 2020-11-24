2d41fc6d99d15812c08d5a5b6cb39e450ef395ff
Matthieu Dorier <mdorier@anl.gov>
2020-11-24 15:53:31 +0000

    added bedrock package

---
+++ packages/mochi-bedrock/package.py
@@ -0,0 +1,30 @@
+from spack import *
+
+
+class MochiBedrock(CMakePackage):
+    """Mochi bootstrapping system"""
+
+    homepage = "https://xgitlab.cels.anl.gov/sds/bedrock"
+    url = "https://xgitlab.cels.anl.gov/sds/bedrock"
+    git = 'https://xgitlab.cels.anl.gov/sds/bedrock.git'
+
+    version('develop', branch='master')
+    version('master', branch='master', preferred=True)
+
+    depends_on('mochi-thallium')
+    depends_on('mochi-margo@master')
+    depends_on('mochi-ssg')
+    depends_on('mochi-abt-io')
+
+    depends_on('mochi-thallium@develop', when='@develop')
+    depends_on('mochi-margo@develop', when='@develop')
+    depends_on('mochi-ssg@develop', when='@develop')
+    depends_on('mochi-abt-io@develop', when='@develop')
+
+    depends_on('nlohmann-json')
+    depends_on('spdlog')
+    depends_on('tclap')
+
+    def cmake_args(self):
+        extra_args = ['-DBUILD_SHARED_LIBS=ON']
+        return extra_args
