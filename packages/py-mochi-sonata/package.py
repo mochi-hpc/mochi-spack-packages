51ccf1ce3b54093fd2372880aeaeb8154566eafb
Matthieu Dorier <mdorier@anl.gov>
2020-12-07 12:47:24 +0000

    added new version of py-sonata

--- packages/py-mochi-sonata/package.py
+++ packages/py-mochi-sonata/package.py
@@ -34,6 +34,7 @@ class PyMochiSonata(PythonPackage):
 
     version('develop', branch="master")
     version('master', branch="master")
+    version('0.1.2', tag='v0.1.2')
     version('0.1.1', tag='v0.1.1')
     version('0.1', tag='v0.1')
 
