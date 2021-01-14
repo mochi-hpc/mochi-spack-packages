d1b1a347b8239cb9cc1fdeb5fb9b8cf828426284
Phil Carns <carns@mcs.anl.gov>
2021-01-14 17:25:29 -0500

    change name of bedrock dev version
    - now head-bedrock instead of develop-bedrock
    - this prevents the package from triggering @develop versions on all
      dependencies because of the "develop" root name

--- packages/mochi-bake/package.py
+++ packages/mochi-bake/package.py
@@ -11,7 +11,7 @@ class MochiBake(AutotoolsPackage):
     version('develop', branch='master')
     version('master', branch='master')
     # branch with bedrock support under development
-    version('develop-bedrock', branch='dev-bedrock-26')
+    version('head-bedrock', branch='dev-bedrock-26')
     # branch with misc experimental features combined for testing
     version('head-carns-exp', branch='carns/dev-experimental')
     version('0.5', tag='v0.5.0')
