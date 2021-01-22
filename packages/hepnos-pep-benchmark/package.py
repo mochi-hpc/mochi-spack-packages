902248a87f5b89f7dad776d7f34ac56a27a91534
Matthieu Dorier <mdorier@anl.gov>
2021-01-22 17:53:09 +0000

    added parallel event processing benchmark as an hepnos package

---
+++ packages/hepnos-pep-benchmark/package.py
@@ -0,0 +1,18 @@
+from spack import *
+
+
+class HepnosPepBenchmark(CMakePackage):
+    """Parallel event processing benchmark for HEPnOS."""
+
+    homepage = "https://xgitlab.cels.anl.gov/sds/hep/HEPnOS-PEP-Benchmark"
+    url = "https://xgitlab.cels.anl.gov/sds/hep/HEPnOS-PEP-Benchmark"
+    git = 'https://xgitlab.cels.anl.gov/sds/hep/HEPnOS-PEP-Benchmark.git'
+
+    version('develop', branch='master', submodules=True)
+    version('master', branch='master', submodules=True)
+
+    depends_on('cmake@3.11.0:', type=('build'))
+    depends_on('mpi')
+    depends_on('hepnos@0.4:')
+    depends_on('tclap')
+    depends_on('spdlog')
