ade83037e0852f19e0f47cc2a60e9b3d9e923ad7
Rob Latham <robl@mcs.anl.gov>
2021-02-26 15:36:06 -0600

    add an 'hdf5' variant to darshan

---
+++ packages/darshan-runtime/package.py
@@ -0,0 +1,18 @@
+
+from spack.pkg.builtin.darshan_runtime import DarshanRuntime
+
+
+class DarshanRuntime(DarshanRuntime):
+
+    variant('hdf5', default=False, description='additional HDF5 statistics')
+
+    depends_on('hdf5@1.10.0:', when='+hdf5')
+
+    def configure_args(self):
+        spec = self.spec
+        config_args = super(DarshanRuntime, self).configure_args()
+
+        if '+hdf5' in spec:
+            config_args.append('--enable-hdf5-mod')
+
+        return config_args
