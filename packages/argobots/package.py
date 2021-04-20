16005ced16e421b8a3d2b5758188a7cea52bc1c8
Phil Carns <carns@mcs.anl.gov>
2021-04-20 15:28:16 -0400

    mimic variants in upstream argobots
    - making these available for mochi users with older upstream spack
      releases

--- packages/argobots/package.py
+++ packages/argobots/package.py
@@ -10,18 +10,50 @@ class Argobots(Argobots):
     version("1.0.1", sha256="fa05a02d7f8f74d845647636609219ee02f6adf628ebcbf40393f829987d9036")
     version("1.0", sha256="36a0815f7bf99900a9c9c1eef61ef9b3b76aa2cfc4594a304f6c8c3296da8def")
 
-    variant('stack-unwind', default=False, description='Enable stack unwinding')
-    conflicts('+stack-unwind', when='@:1.0.1',
-              msg='+stack-unwind variant only available for @1.1 or newer')
+    # some of these may be duplicated upstream; we want to cover them so
+    # that they are available when using the mochi spack repo
+
+    variant("perf", default=True, description="Add performance optimization flags")
+    variant("valgrind", default=False, description="Enable Valgrind")
+    variant("debug", default=False, description="Compiled with debugging symbols")
+    variant("stackunwind", default=False, description="Enable function stack unwinding")
+    variant("stackguard", default="no", description="Enable stack guard",
+            values=('no', 'canary-32', 'mprotect', 'mprotect-strict'), multi=False)
+    variant("tool", default=False, description="Enable ABT_tool interface")
+    variant("affinity", default=False, description="Enable affinity setting")
+
+    depends_on("valgrind", when="+valgrind")
     depends_on("libunwind", when="+stack-unwind")
 
     def configure_args(self):
         spec = self.spec
-        config_args = super(Argobots, self).configure_args()
+        args = super(Argobots, self).configure_args()
 
-        if '+stack-unwind' in spec:
-            config_args.append('--enable-stack-unwind')
+        if '+perf' in self.spec:
+            args.append('--enable-perf-opt')
+
+        if '+valgrind' in self.spec:
+            args.append('--enable-valgrind')
         else:
-            config_args.append('--disable-stack-unwind')
+            args.append('--disable-valgrind')
 
-        return config_args
+        if '+debug' in self.spec:
+            args.append('--enable-debug=yes')
+        else:
+            args.append('--disable-debug')
+
+        if '+stackunwind' in self.spec:
+            args.append('--enable-stack-unwind')
+            args.append('--with-libunwind={0}'.format(self.spec['libunwind'].prefix))
+
+        stackguard = self.spec.variants['stackguard'].value
+        if stackguard != 'no':
+            args.append('--enable-stack-overflow-check={0}'.format(stackguard))
+
+        if '+tool' in self.spec:
+            args.append('--enable-tool')
+
+        if '+affinity' in self.spec:
+            args.append('--enable-affinity')
+
+        return args
