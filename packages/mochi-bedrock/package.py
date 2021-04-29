1e4694aaf124ebd7a0ae913767ea1a5f56c39d63
Matthieu Dorier <mdorier@anl.gov>
2021-04-29 12:47:34 +0100

    added new version of bedrock

--- packages/mochi-bedrock/package.py
+++ packages/mochi-bedrock/package.py
@@ -5,13 +5,14 @@ class MochiBedrock(CMakePackage):
     """Mochi bootstrapping system"""
 
     homepage = 'https://github.com/mochi-hpc/mochi-bedrock'
-    url = 'https://github.com/mochi-hpc/mochi-bedrock'
+    url = 'https://github.com/mochi-hpc/mochi-bedrock/archive/refs/tags/v0.2.1.tar.gz'
     git = 'https://github.com/mochi-hpc/mochi-bedrock.git'
 
     version('develop', branch='main')
     version('main', branch='main')
     version('master', branch='main')
-    version('0.2', tag='v0.2', preferred=True)
+    version('0.2.1', sha256='cde4f8ecac4d765dba5255d26a5e859460c7f0a2b29dcdffb44866119116ae2c')
+    version('0.2', tag='v0.2')
     version('0.1', tag='v0.1')
 
     depends_on('mochi-margo@0.9:', when='@0.1:9.9.9')
