a567b7db82df70ed4e1fbd4bacbf2997a85aa959
Phil Carns <carns@mcs.anl.gov>
2021-07-27 10:37:06 -0400

    make ucx-enabled build of Mercury available
    spack install mercury@master-ucx+ucx

--- packages/mercury/package.py
+++ packages/mercury/package.py
@@ -12,3 +12,63 @@ class Mercury(Mercury):
     version('2.0.0',
            sha256='9e80923712e25df56014309df70660e828dbeabbe5fcc82ee024bcc86e7eb6b7')
     version('2.0.0rc1-pvar', git='https://github.com/srini009/mercury.git', branch='mercury_profiling_interface')
+    version('master-ucx', branch='ucx', submodules=True)
+
+    variant('ucx', default=False, description='Use UCX plugin')
+
+    depends_on('ucx', when='+ucx')
+
+    def cmake_args(self):
+        """Populate cmake arguments for Mercury."""
+        spec = self.spec
+        variant_bool = lambda feature: str(feature in spec)
+        parallel_tests = '+mpi' in spec and self.run_tests
+
+        cmake_args = [
+            '-DBUILD_SHARED_LIBS:BOOL=%s' % variant_bool('+shared'),
+            '-DBUILD_TESTING:BOOL=%s' % str(self.run_tests),
+            '-DMERCURY_ENABLE_PARALLEL_TESTING:BOOL=%s' % str(parallel_tests),
+            '-DMERCURY_USE_BOOST_PP:BOOL=ON',
+            '-DMERCURY_USE_CHECKSUMS:BOOL=%s' % variant_bool('+checksum'),
+            '-DMERCURY_USE_SYSTEM_MCHECKSUM:BOOL=OFF',
+            '-DMERCURY_USE_XDR:BOOL=OFF',
+            '-DNA_USE_UCX:BOOL=%s' % variant_bool('+ucx'),
+            '-DNA_USE_BMI:BOOL=%s' % variant_bool('+bmi'),
+            '-DNA_USE_CCI:BOOL=%s' % variant_bool('+cci'),
+            '-DNA_USE_MPI:BOOL=%s' % variant_bool('+mpi'),
+            '-DNA_USE_SM:BOOL=%s'  % variant_bool('+sm'),
+        ]
+
+        if '@2.0.0:' in spec:
+            cmake_args.extend([
+                '-DMERCURY_ENABLE_DEBUG:BOOL=%s' % variant_bool('+debug'),
+            ])
+
+        # Previous versions of mercury had more extensive CMake options
+        if '@:1.0.1' in spec:
+            cmake_args.extend([
+                '-DMERCURY_ENABLE_POST_LIMIT:BOOL=OFF',
+                '-DMERCURY_ENABLE_VERBOSE_ERROR=%s' % variant_bool('+debug'),
+                '-DMERCURY_USE_EAGER_BULK:BOOL=ON',
+                '-DMERCURY_USE_SELF_FORWARD:BOOL=ON',
+            ])
+
+        if '@1.0.0:' in spec:
+            cmake_args.extend([
+                '-DMERCURY_USE_SYSTEM_BOOST:BOOL=%s'
+                % variant_bool('+boostsys'),
+                '-DNA_USE_OFI:BOOL=%s' % variant_bool('+ofi'),
+            ])
+
+        if '+ofi' in spec:
+            cmake_args.append(
+                '-DNA_OFI_GNI_USE_UDREG:BOOL=%s' % variant_bool('+udreg')
+            )
+            if self.run_tests:
+                cmake_args.append(
+                    '-DNA_OFI_TESTING_PROTOCOL:STRING={0}'.format(
+                        ';'.join(spec['libfabric'].variants['fabrics'].value)
+                    )
+                )
+
+        return cmake_args
