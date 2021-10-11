2b733a3e5f1dd400c2bb72ec466e2e9c874f2b7d
Matthieu Dorier <mdorier@anl.gov>
2021-10-11 16:40:44 +0000

    added yokan package

---
+++ packages/mochi-yokan/package.py
@@ -0,0 +1,75 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+from spack import *
+
+
+class MochiYokan(CMakePackage, PythonPackage):
+    """A Mochi library that provides a Margo-based key/value service."""
+
+    homepage = "https://github.com/mochi-hpc/mochi-yokan"
+    url = "https://github.com/mochi-hpc/mochi-yokan/archive/refs/tags/v0.1.tar.gz"
+    git = "https://github.com/mochi-hpc/mochi-yokan.git"
+
+    version('develop', branch='main')
+    version('main', branch='main', preferred=True)
+
+    variant('berkeleydb', default=False, description="Enable BerkelyDB backend")
+    variant('leveldb', default=False, description="Enable LevelDB backend")
+    variant('lmdb', default=False, description="Enable LMDB backend")
+    variant('rocksdb', default=False, description="Enable RocksDB backend")
+    variant('tkrzw', default=False, description="Enable TKRZW backend")
+    variant('gdbm', default=False, description="Enable GDBM backend")
+    variant('unqlite', default=False, description="Enable Unqlite backend")
+    variant('lua', default=False, description="Enable Lua filtering")
+    variant('python', default=False, description="Enable Python binding")
+    variant('bedrock', default=False, description="Enable Bedrock support")
+
+    depends_on('pkgconfig')
+    depends_on('uuid')
+    depends_on('nlohmann-json')
+
+    # mochi dependencies
+    depends_on('mochi-margo@0.9.5:')
+    depends_on('mochi-bedrock', when="+bedrock")
+    depends_on('py-mochi-margo@0.4:', when='+python')
+
+    # mochi dependencies for develop version
+    depends_on('mochi-margo@develop', when='@develop')
+    depends_on('mochi-bedrock@develop', when="+bedrock @develop")
+    depends_on('py-mochi-margo@develop', when='+python')
+
+    # backends
+    depends_on('berkeley-db @18.1.40: +cxx +stl', when='+berkeleydb')
+    # For now we are stuck with leveldb 1.22 because of a problem with 1.23
+    # (see https://github.com/google/leveldb/issues/891)
+    depends_on('leveldb@:1.22', when='+leveldb')
+    depends_on('lmdb', when='+lmdb')
+    depends_on('rocksdb', when='+rocksdb')
+    depends_on('tkrzw', when='+tkrzw')
+    depends_on('gdbm', when='+gdbm')
+    depends_on('unqlite@master', when='+unqlite')
+
+    # additional dependencies
+    depends_on('lua-sol2', when='+lua')
+    depends_on('python@3.6.0:', when='+python')
+    depends_on('py-pybind11@2.7.0:', when='+python')
+
+    def cmake_args(self):
+        args = []
+        variant_bool = lambda feature: str(feature in self.spec)
+        args.append('-DENABLE_BERKELEYDB:BOOL=%s' % variant_bool('+berkeleydb'))
+        args.append('-DENABLE_LEVELDB:BOOL=%s' % variant_bool('+leveldb'))
+        args.append('-DENABLE_LMDB:BOOL=%s' % variant_bool('+lmdb'))
+        args.append('-DENABLE_ROCKSDB:BOOL=%s' % variant_bool('+rocksdb'))
+        args.append('-DENABLE_TKRZW:BOOL=%s' % variant_bool('+tkrzw'))
+        args.append('-DENABLE_GDBM:BOOL=%s' % variant_bool('+gdbm'))
+        args.append('-DENABLE_UNQLITE:BOOL=%s' % variant_bool('+unqlite'))
+        args.append('-DENABLE_LUA:BOOL=%s' % variant_bool('+lua'))
+        args.append('-DENABLE_PYTHON:BOOL=%s' % variant_bool('+python'))
+        args.append('-DENABLE_BEDROCK:BOOL=%s' % variant_bool('+bedrock'))
+        return args
+
+    def install(self, spec, prefix):
+        CMakePackage.install(self, spec, prefix)
