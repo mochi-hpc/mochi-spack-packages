ec3e71ba5128bf4df3d669bbbdace4e6e00733a0
Phil Carns <carns@mcs.anl.gov>
2021-10-25 14:24:02 -0400

    make mercury 2.1.0rc2 available for testing
    - leave 2.0.1 as preferred for now

--- packages/mercury/package.py
+++ packages/mercury/package.py
@@ -8,7 +8,8 @@ from spack.pkg.builtin.mercury import Mercury
 class Mercury(Mercury):
 
     git = 'https://github.com/mercury-hpc/mercury.git'
-    version('2.0.1', sha256='335946d9620ac669643ffd9861a5fb3ee486834bab674b7779eaac9d6662e3fa')
+    version('2.1.0rc2', sha256='921248fbcaf33ca3fa7d6678982e6132b880a8cc85f20e4c7cbf66508dc0e0be')
+    version('2.0.1', sha256='335946d9620ac669643ffd9861a5fb3ee486834bab674b7779eaac9d6662e3fa', preferred=True)
     version('2.0.0',
            sha256='9e80923712e25df56014309df70660e828dbeabbe5fcc82ee024bcc86e7eb6b7')
     version('2.0.0rc1-pvar', git='https://github.com/srini009/mercury.git', branch='mercury_profiling_interface')
