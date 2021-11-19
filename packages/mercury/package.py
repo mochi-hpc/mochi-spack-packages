363b7c821ff27825aad559f29e2cb701a6bd66c3
Phil Carns <carns@mcs.anl.gov>
2021-11-19 12:42:39 -0500

    bump mercury to 2.1.0rc4

--- packages/mercury/package.py
+++ packages/mercury/package.py
@@ -8,6 +8,7 @@ from spack.pkg.builtin.mercury import Mercury
 class Mercury(Mercury):
 
     git = 'https://github.com/mercury-hpc/mercury.git'
+    version('2.1.0rc4', sha256='64e1985f35d1ab63cbfbd251b96e2acd03e370ce261d13dd017f143c1ff16a90')
     version('2.1.0rc3', sha256='e54306cb193c9d9de33688a6f6947b66130ab6b22bbf7ff8f2934cd6801ee081')
     version('2.1.0rc2', sha256='921248fbcaf33ca3fa7d6678982e6132b880a8cc85f20e4c7cbf66508dc0e0be')
     version('2.0.1', sha256='335946d9620ac669643ffd9861a5fb3ee486834bab674b7779eaac9d6662e3fa')
