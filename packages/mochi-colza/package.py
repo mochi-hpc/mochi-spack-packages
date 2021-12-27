9e3bd6adc2fbf068745e27f19767d94d5f05cccb
Matthieu Dorier <mdorier@anl.gov>
2021-12-27 13:24:40 +0000

    corrected dependencies for mochi-colza

--- packages/mochi-colza/package.py
+++ packages/mochi-colza/package.py
@@ -30,7 +30,7 @@ class MochiColza(CMakePackage):
     depends_on('mochi-thallium @0.8:')
     depends_on('mochi-mona')
     depends_on('mochi-bedrock', when='+bedrock')
-    depends_on('mochi-ssg @0.4.5:0.4.6', when='@0.1:9.9.9')
+    depends_on('mochi-ssg @0.4.5', when='@0.1:9.9.9')
     depends_on('mochi-ssg @0.4.6', when='@main')
 
     # dependencies for develop version
