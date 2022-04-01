77a38d70f1f54d77dd3056f1ecdeb463a727ecc4
Matthieu Dorier <mdorier@anl.gov>
2022-04-01 08:54:37 +0000

    added new version of the hepnos-dataloader

--- packages/hepnos-dataloader/package.py
+++ packages/hepnos-dataloader/package.py
@@ -34,6 +34,7 @@ class HepnosDataloader(CMakePackage):
 
     version('develop', branch='main', submodules=True)
     version('main', branch='main', submodules=True)
+    version('0.5.2', tag='v0.5.2', submodules=True)
     version('0.5.1', tag='v0.5.1', submodules=True)
     version('0.5', tag='v0.5', submodules=True)
     version('0.4', tag='v0.4', submodules=True)
