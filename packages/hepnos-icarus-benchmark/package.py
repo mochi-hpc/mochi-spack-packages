cc04d8adb8ed1d03d44bc9fcfc5112422a30906b
Matthieu Dorier <mdorier@anl.gov>
2022-08-02 14:37:36 +0000

    added hepnos-icarus-benchmark package

---
+++ packages/hepnos-icarus-benchmark/package.py
@@ -0,0 +1,23 @@
+from spack import *
+
+
+class HepnosIcarusBenchmark(CMakePackage):
+    """Benchmark for HEPnOS that mimics the ICARUS application."""
+
+    homepage = "https://github.com/hepnos/HEPnOS-ICARUS-Benchmark"
+    url = "https://github.com/hepnos/HEPnOS-ICARUS-Benchmark"
+    git = "https://github.com/hepnos/HEPnOS-ICARUS-Benchmark.git"
+
+    version('develop', branch='main')
+    version('main', branch='main', preferred=True)
+
+    depends_on('cmake@3.11.0:', type=('build'))
+    depends_on('mpi')
+    depends_on('hepnos@0.6.6:')
+    depends_on('tclap')
+    depends_on('spdlog')
+
+    def cmake_args(self):
+        extra_args = ['-DBUILD_SHARED_LIBS=ON',
+                      '-DCMAKE_CXX_COMPILER=%s' % self.spec['mpi'].mpicxx]
+        return extra_args
