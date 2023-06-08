4a8d9d1f2ee69022622a23b1f34532f41d502003
Matthieu Dorier <mdorier@anl.gov>
2023-06-08 14:57:13 +0000

    new version of isonata

--- packages/mochi-isonata/package.py
+++ packages/mochi-isonata/package.py
@@ -15,18 +15,21 @@ class MochiIsonata(CMakePackage):
     git = "https://github.com/mochi-hpc/mochi-isonata.git"
 
     version("main", branch="main")
+    version("0.2.0", sha256="2266b2afac2de7befb15b696c9613b1b7a25a700c378687feab5644cabde7b8c")
     version("0.1.0", sha256="86ea6e0da391de9ff30023dc43096a1bf6ffb5ca1979a5371e0f37e24430dec4")
 
     variant("sonata", default=True, description="Enable Sonata backend")
+    variant("yokan", default=True, description="Enable Yokan backend",
+            when="@0.2.0:")
 
     depends_on("mochi-thallium")
     depends_on("nlohmann-json")
     depends_on("mochi-sonata@0.6.3:", when="+sonata")
+    depends_on("mochi-yokan@0.2.11:", when="+yokan")
 
     def cmake_args(self):
-        args = []
-        if "+sonata" in self.spec:
-            args.append("-DENABLE_SONATA=ON")
-        else:
-            args.append("-DENABLE_SONATA=OFF")
+        args = [
+            self.define_from_variant("ENABLE_SONATA", "sonata"),
+            self.define_from_variant("ENABLE_YOKAN", "yokan")
+        ]
         return args
