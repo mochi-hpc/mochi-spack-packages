470c09e13285eaed1265d4aca02bb173c60cd5eb
Matthieu Dorier <mdorier@anl.gov>
2023-08-01 09:18:54 +0000

    edited poesie package

--- packages/mochi-poesie/package.py
+++ packages/mochi-poesie/package.py
@@ -28,31 +28,33 @@ from spack import *
 class MochiPoesie(CMakePackage):
     """A Mochi library that provides a Margo-based embedded scripting service"""
 
-    homepage = 'https://github.com/mochi-hpc/mochi-poesie'
-    url = 'https://github.com/mochi-hpc/mochi-poesie/archive/refs/tags/v0.2.tar.gz'
-    git = 'https://github.com/mochi-hpc/mochi-poesie.git'
+    homepage = "https://github.com/mochi-hpc/mochi-poesie"
+    url = "https://github.com/mochi-hpc/mochi-poesie/archive/refs/tags/v0.2.tar.gz"
+    git = "https://github.com/mochi-hpc/mochi-poesie.git"
 
-    version('develop', branch='main')
-    version('main', branch='main')
+    version("develop", branch="main")
+    version("main", branch="main")
     version("0.2", sha256="dafbdcdd8feb8d81f0dd31cc329c3656e22c8381b5c6a628e57567822190e9d3")
 
-    variant('lua',    default=True, description="Enable Lua interpreters")
-    variant('python', default=True, description="Enable Python interpreters")
-    variant('bedrock', default=False, description="Enable Bedrock support")
+    variant("lua", default=True, description="Enable Lua interpreters")
+    variant("python", default=True, description="Enable Python interpreters")
+    variant("js", default=True, description="Enable Javascript interpreters", when="@main")
+    variant("bedrock", default=False, description="Enable Bedrock support")
 
-    depends_on('mochi-margo@0.9:')
-    depends_on('mochi-margo@develop', when='@develop')
-    depends_on('mochi-bedrock', when='+bedrock')
-    depends_on('mochi-bedrock@develop', when='+bedrock @develop')
+    depends_on("mochi-margo@0.9:")
+    depends_on("mochi-margo@develop", when="@develop")
+    depends_on("mochi-bedrock", when="+bedrock")
+    depends_on("mochi-bedrock@develop", when="+bedrock @develop")
 
-    depends_on('json-c')
-    depends_on('lua', when="+lua")
-    depends_on('python@3.6:', when="+python")
+    depends_on("json-c")
+    depends_on("quickjs", when="+js")
+    depends_on("lua", when="+lua")
+    depends_on("python@3.6:", when="+python")
 
     def cmake_args(self):
-        args = []
-        variant_bool = lambda feature: str(feature in self.spec)
-        args.append('-DENABLE_PYTHON:BOOL=%s' % variant_bool('+python'))
-        args.append('-DENABLE_LUA:BOOL=%s' % variant_bool('+lua'))
-        args.append('-DENABLE_BEDROCK:BOOL=%s' % variant_bool('+bedrock'))
-        return args
+        cmake_args = [
+            self.define_from_variant("ENABLE_PYTHON", "python"),
+            self.define_from_variant("ENABLE_LUA", "lua"),
+            self.define_from_variant("ENABLE_JAVASCRIPT", "js"),
+        ]
+        return cmake_args
