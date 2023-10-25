ee590418b6c4bf09ae05a9bd5e270e7938ea3c85
Matthieu Dorier <mdorier@anl.gov>
2023-10-25 15:44:03 +0100

    added warabi version 0.1.0

--- packages/mochi-warabi/package.py
+++ packages/mochi-warabi/package.py
@@ -3,22 +3,6 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install mochi-warabi
-#
-# You can edit this file again by typing:
-#
-#     spack edit mochi-warabi
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack.package import *
 
@@ -26,13 +10,15 @@ from spack.package import *
 class MochiWarabi(CMakePackage):
     """Warabi is a BLOB-storage microservice for Mochi."""
 
+    url = "https://github.com/mochi-hpc/mochi-warabi/archive/refs/tags/v0.1.0.tar.gz"
     homepage = "https://github.com:/mochi-hpc/mochi-warabi"
     git = "https://github.com:/mochi-hpc/mochi-warabi.git"
 
     maintainers("mdorier")
 
-    version("main", branch="main", preferred=True)
+    version("main", branch="main")
     version("develop", branch="main")
+    version("0.1.0", sha256="a327f1a0c63e5e913b1d16e9cd73590e9f7b675acd187d9938cb3dcc5210486f")
 
     variant("bedrock", default=False, description="Enable Bedrock support")
     variant("remi", default=False, description="Enable REMI support")
