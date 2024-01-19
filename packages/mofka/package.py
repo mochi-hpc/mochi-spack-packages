e6ea9a7c9890c854b9db48db604765e903639f63
Matthieu Dorier <mdorier@anl.gov>
2024-01-19 15:40:41 +0000

    mofka version 0.0.2 released

--- packages/mofka/package.py
+++ packages/mofka/package.py
@@ -18,6 +18,7 @@ class Mofka(CMakePackage):
 
     version("main", branch="main")
     version("0.0.1", sha256="ba58521053ab3c1d6ba08614257c11c7e15e51636c2fc24415fcf8cf20451365")
+    version("0.0.2", sha256="024c18be993a3fe6cde6918b81486c2a9727050da060800a92a634355d8ad83d")
 
     depends_on("pkg-config", type=("build",))
     depends_on("uuid")
@@ -27,7 +28,9 @@ class Mofka(CMakePackage):
     depends_on("fmt")
     depends_on("tclap")
     depends_on("mochi-thallium")
-    depends_on("mochi-bedrock@0.6.0:+ssg")
+    depends_on("mochi-bedrock@0.8.3:+ssg")
+    depends_on("mochi-yokan@0.2.0:+bedrock", when="@0.0.2:")
+    depends_on("mochi-warabi@0.4.2:+bedrock", when="@0.0.2:")
 
     def cmake_args(self):
         args = ["-DENABLE_BAKE=OFF"]
