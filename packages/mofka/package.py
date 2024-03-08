5018c3c8a501ae5baf73460ed8963a8d214c17e3
Matthieu Dorier <mdorier@anl.gov>
2024-03-08 10:04:33 +0000

    added mofka 0.0.4

--- packages/mofka/package.py
+++ packages/mofka/package.py
@@ -18,6 +18,7 @@ class Mofka(CMakePackage):
 
     version("main", branch="main")
     version("develop", branch="main")
+    version("0.0.4", sha256="020d320834dd2e39c11f354a8a5255424ab942bd63ef4baa1f048c22d8c3367d")
     version("0.0.3", sha256="429beac2735c0a75cacf39d1523d26772ad9ddaa42552ba972a033f1cc8f7b35")
     version("0.0.2", sha256="024c18be993a3fe6cde6918b81486c2a9727050da060800a92a634355d8ad83d")
     version("0.0.1", sha256="ba58521053ab3c1d6ba08614257c11c7e15e51636c2fc24415fcf8cf20451365")
@@ -25,27 +26,34 @@ class Mofka(CMakePackage):
     variant("python", default=True, when="@0.0.3:",
             description="Enable python support")
 
+    extends("python", when="+python")
+
     depends_on("cmake@3.21:", type=("build",))
     depends_on("pkg-config", type=("build",))
     depends_on("uuid")
     depends_on("spdlog+fmt_external")
     depends_on("fmt")
-    depends_on("tclap")
-    depends_on("nlohmann-json", when="@main,develop")
-    depends_on("nlohmann-json-schema-validator", type=("build",), when="@main,develop")
-    depends_on("rapidjson", when="@:0.0.3")
-    depends_on("valijson", when="@:0.0.3")
+    depends_on("tclap", type=("build",))
+    depends_on("nlohmann-json", when="@0.0.4:")
+    depends_on("nlohmann-json-schema-validator", type=("build",), when="@0.0.4:")
     depends_on("mochi-thallium")
     depends_on("mochi-bedrock@0.8.3:0.9.2+ssg", when="@:0.0.2")
     depends_on("mochi-bedrock@0.10.0:+ssg", when="@0.0.3:")
     depends_on("mochi-bedrock+python", when="+python")
     depends_on("mochi-yokan@0.2.0:+bedrock", when="@0.0.2:")
-    depends_on("mochi-warabi@0.4.2:+bedrock", when="@0.0.2:")
+    depends_on("mochi-yokan@0.4.2:+bedrock", when="@0.0.4:")
+    depends_on("mochi-warabi@0.3.0:+bedrock", when="@0.0.2:")
+    depends_on("py-pybind11", type=("build",), when="+python")
+    depends_on("py-mochi-margo", when="+python")
+
+    depends_on("rapidjson", when="@:0.0.3")
+    depends_on("valijson", when="@:0.0.3")
 
     depends_on("mochi-thallium@develop", when="@develop")
     depends_on("mochi-bedrock@develop", when="@develop")
     depends_on("mochi-yokan@develop", when="@develop")
     depends_on("mochi-warabi@develop", when="@develop")
+    depends_on("py-mochi-margo@develop", when="@develop +python")
 
     def cmake_args(self):
         args = ["-DENABLE_BAKE=OFF"]
