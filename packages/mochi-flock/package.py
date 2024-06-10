9d212325865dbb2fd9718766e9712610a55d4d2e
Matthieu Dorier <mdorier@anl.gov>
2024-06-10 17:24:00 +0100

    added version 0.2.0 of flock

--- packages/mochi-flock/package.py
+++ packages/mochi-flock/package.py
@@ -19,22 +19,29 @@ class MochiFlock(CMakePackage):
     version("main", branch="main")
     version("develop", branch="main")
     version("0.1.0", sha256="5bf3a87ad3f1408f322e9e777f96ad7a82c26b93dcf1df6ba4bffa11a55dc2ee")
+    version("0.2.0", sha256="fce7ecb61f7ec9f8e3f5929427943d001ce66800979d4b125f9647162004d6b6")
 
     variant("bedrock", default=False, description="Enable Bedrock support")
     variant("mpi", default=False, description="Enable MPI support")
+    variant("python", default=False, description="Enable Python support")
 
     depends_on("pkgconfig", type=("build",))
-    depends_on("mochi-margo")
     depends_on("json-c")
-
+    depends_on("py-pybind11", when="@0.2.0: +python")
+    depends_on("mochi-margo")
+    depends_on("mochi-thallium", when="@0.2.0:")
+    depends_on("py-mochi-margo", when="@0.2.0: +python")
     depends_on("mochi-bedrock", when="+bedrock")
 
     depends_on("mochi-margo@develop", when="@develop")
     depends_on("mochi-bedrock@develop", when="@develop +bedrock")
+    depends_on("mochi-thallium@develop", when="@develop")
+    depends_on("py-mochi-margo@develop", when="@develop +python")
 
     def cmake_args(self):
         args = []
         variant_bool = lambda feature: str(feature in self.spec)
         args.append("-DENABLE_BEDROCK:BOOL=%s" % variant_bool("+bedrock"))
         args.append("-DENABLE_MPI:BOOL=%s" % variant_bool("+mpi"))
+        args.append("-DENABLE_PYTHON:BOOL=%s" % variant_bool("+python"))
         return args
