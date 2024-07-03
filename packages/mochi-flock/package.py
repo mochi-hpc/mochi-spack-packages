c4222879b6dd23700c120bde23ade7f16a248809
Matthieu Dorier <mdorier@anl.gov>
2024-07-03 16:18:33 +0100

    added new version of flock

--- packages/mochi-flock/package.py
+++ packages/mochi-flock/package.py
@@ -18,12 +18,13 @@ class MochiFlock(CMakePackage):
 
     version("main", branch="main")
     version("develop", branch="main")
+    version("0.3.0", sha256="e7aa78e01ce952731a91a703275deb398b46280e960d35227e350b9fb238dc38")
     version("0.2.2", sha256="7b66503b9bd96245ec918c97f142443969725bb151c761dc2cbd41f583b0417c")
     version("0.2.1", sha256="0cd25bc32ffccc5a4249c49d2a874b9d1a1a7f15d428b17e3ecadc6e2661a14d")
     version("0.2.0", sha256="fce7ecb61f7ec9f8e3f5929427943d001ce66800979d4b125f9647162004d6b6")
     version("0.1.0", sha256="5bf3a87ad3f1408f322e9e777f96ad7a82c26b93dcf1df6ba4bffa11a55dc2ee")
 
-    variant("bedrock", default=False, description="Enable Bedrock support")
+    variant("bedrock", default=True, description="Enable Bedrock support")
     variant("mpi", default=False, description="Enable MPI support")
     variant("python", default=False, description="Enable Python support")
 
@@ -33,7 +34,8 @@ class MochiFlock(CMakePackage):
     depends_on("mochi-margo")
     depends_on("mochi-thallium", when="@0.2.0:")
     depends_on("py-mochi-margo", when="@0.2.0: +python")
-    depends_on("mochi-bedrock", when="+bedrock")
+    depends_on("mochi-bedrock", when="+bedrock @:0.2.2")
+    depends_on("mochi-bedrock-module-api", when="+bedrock @0.3.0:")
 
     depends_on("mochi-margo@develop", when="@develop")
     depends_on("mochi-bedrock@develop", when="@develop +bedrock")
