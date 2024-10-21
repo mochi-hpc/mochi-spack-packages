12aea560f43e6ba64000ea9f1bc4c6e700605804
Matthieu Dorier <mdorier@anl.gov>
2024-10-21 19:48:38 +0100

    added margo version 0.18.2

--- packages/mochi-margo/package.py
+++ packages/mochi-margo/package.py
@@ -26,8 +26,8 @@ class MochiMargo(AutotoolsPackage):
     version('develop', branch='main')
     # NOTE: forcing 0.18.1 to preferred to make sure it takes precedence
     # over the hg-event branch unless its explicitly asked for
-    version("0.18.1",
-    sha256="06221986deaa5eb20001c49f29d580722a16b5bde66c1333b3b02f677ef973b5", preferred=True)
+    version("0.18.2", sha256="a3a9fde826954be06b9123887533f91e6725faf6f6c682c080b97c2172a22057")
+    version("0.18.1", sha256="06221986deaa5eb20001c49f29d580722a16b5bde66c1333b3b02f677ef973b5")
     version("0.18.1-hg-event", branch='carns/dev-hg-event')
     version("0.18.0", sha256="5b3e8b64217490bd8643506699cd06538abaf1bb19eb0429506de62bf0c8402e")
     version("0.17.3", sha256="286ec8bab62e8f21b1d1acb0afa6699be247504de783897433b5d81ef3b5fe18")
