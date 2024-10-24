c360a2576c0d832ea821ea41c9d70dd6fa967d87
Matthieu Dorier <mdorier@anl.gov>
2024-10-24 15:07:07 +0100

    new version of mofka

--- packages/mofka/package.py
+++ packages/mofka/package.py
@@ -18,6 +18,7 @@ class Mofka(CMakePackage):
 
     version("main", branch="main")
     version("develop", branch="main")
+    version("0.3.2", sha256="6fcbe81487093e111cba82d7d7fd8ec82190e27f8309446e0164d6ff84608ff3")
     version("0.3.1", sha256="b60811e023ad57ada70b68ea1dbabeeac020930b72a37013fec6b36827db52ce")
     version("0.3.0", sha256="6489051756444a53b175ced040a10a4acaa942df5fe92c1f8e22b45f50537436")
     version("0.2.0", sha256="038ce14a8643ff143311616adf7e0ade6bdf47448012b13e3417e53167f8bbee")
