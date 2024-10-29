2f636ede5624c4b7381ac603a246799bb8ed7ca2
Matthieu Dorier <mdorier@anl.gov>
2024-10-29 10:03:14 +0000

    new version of bedrock

--- packages/mochi-bedrock/package.py
+++ packages/mochi-bedrock/package.py
@@ -12,6 +12,7 @@ class MochiBedrock(CMakePackage):
 
     version("develop", branch="main")
     version("main", branch="main")
+    version("0.15.1", sha256="f2ae48157baa6d84290c93d8ff14f98bcaefd5fd7ceeb4eb56ea38cced1fcb8f")
     version("0.15.0", sha256="48c779d7a043ab12364902585002d2e3eb5e4676854164ff4269c0fbc607c2d5")
     version("0.14.2", sha256="d47bc258500115e96e3287195b00ff7d75d6b236d401041256bd9c9a7d849f87")
     version("0.14.1", sha256="4407885ac15f9c214430c3110df3fc63d189d8339d83eea26b7d3991a298d327")
