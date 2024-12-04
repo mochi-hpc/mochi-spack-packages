1c4d436ddf202a95ea514404410ae9309dbd88ae
Matthieu Dorier <mdorier@anl.gov>
2024-12-04 16:35:56 +0000

    new version of flock

--- packages/mochi-flock/package.py
+++ packages/mochi-flock/package.py
@@ -18,6 +18,7 @@ class MochiFlock(CMakePackage):
 
     version("main", branch="main")
     version("develop", branch="main")
+    version("0.5.1", sha256="5f91515f9997342c8d4f2f212e08f3345d1927c46ffd88cadb27a0dc144cdc7e")
     version("0.5.0", sha256="88cb8d5794d8451c7595f785cbaaf1d57870a44bd86f799f5e48534178a1bcd2")
     version("0.4.1", sha256="d6ec1ebcc9721aeda32dbf18cd7f50f9ce3e2364169bf455df44549c6b2ef55a")
     version("0.4.0", sha256="42508939ac7c65da2627d2af59bdad0e456c12f6593b49a0a45a0e651e56c5f9")
