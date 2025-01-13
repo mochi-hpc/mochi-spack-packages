0b19998687b0fb437a7468222de3d9981e19b760
Matthieu Dorier <mdorier@anl.gov>
2025-01-13 10:12:48 +0000

    temporary fixing pmdk package

---
+++ packages/pmdk/package.py
@@ -0,0 +1,13 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+
+from spack.pkg.builtin.pmdk import Pmdk as BuiltinPmdk
+from spack.package import *
+
+
+class Pmdk(BuiltinPmdk):
+
+    depends_on('gmake', type='build')
