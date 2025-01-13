0929ab5d0b286b5745c042fd496087aa40830aee
Matthieu Dorier <mdorier@anl.gov>
2025-01-13 13:06:11 +0000

    updated mofka package

--- packages/mofka/package.py
+++ packages/mofka/package.py
@@ -61,6 +61,7 @@ class Mofka(CMakePackage):
     depends_on("mochi-bedrock@0.12.0:0.14.2", when="@0.1.1:0.1.2")
     depends_on("mochi-bedrock+mpi", when="+mpi")
 
+    depends_on("mochi-yokan@0.8.0:+bedrock", when="@0.4.1:")
     depends_on("mochi-yokan@0.7.0:+bedrock", when="@0.2.0:")
     depends_on("mochi-yokan@0.6.0:+bedrock", when="@0.1.2:")
     depends_on("mochi-yokan@0.4.2:+bedrock")
