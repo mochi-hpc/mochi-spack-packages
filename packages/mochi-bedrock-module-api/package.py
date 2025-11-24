e5f00cfe8d235ea941b69c5659121748dd151c32
Matthieu Dorier <mdorier@anl.gov>
2025-11-24 11:36:53 +0000

    new version of mochi-bedrock-module-api

--- packages/mochi-bedrock-module-api/package.py
+++ packages/mochi-bedrock-module-api/package.py
@@ -11,6 +11,7 @@ class MochiBedrockModuleApi(CMakePackage):
 
     version("main", branch="main")
     version("develop", branch="main")
+    version("0.2.1", sha256="cfcec755a27eaf2c6732d1cb031d04919f6de1f4d0fe4d09f6ed72c25c008146")
     version("0.2.0", sha256="94c56d64c77932ec06a6ea74beba182dac08eb25a79d7ffe9a51b29adaf3d614")
     version("0.1.0", sha256="44b2622f90a39b23fab59d4ffa15721ff59138fe0f1ceca9491efd9324489a9d")
 
