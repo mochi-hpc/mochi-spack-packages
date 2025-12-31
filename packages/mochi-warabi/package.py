66d493656f95225ee9617d32c7af0ecf60b62803
Matthieu Dorier <mdorier@anl.gov>
2025-12-31 13:47:13 +0000

    new version of warabi

--- packages/mochi-warabi/package.py
+++ packages/mochi-warabi/package.py
@@ -18,6 +18,7 @@ class MochiWarabi(CMakePackage):
 
     version("main", branch="main")
     version("develop", branch="main")
+    version("0.7.0", sha256="a4c696bc1e06d2959d5d2e3673de850bec38c3058981a43205a97e963bca2b1f")
     version("0.6.3", sha256="8e1fbea5ff84271a83f353902e28dab7095daae4a7a94ff621432f6c19283799")
     version("0.6.2", sha256="5d654704a5d96d2c56d69ddaa2fd1edc267b692f590282ddcb107cca14630a93")
     version("0.6.1", sha256="27e4f5bbb31fbd18985beea64a95b25f14f0a3a65f29f49d6d8472ad768f56e3")
@@ -47,6 +48,7 @@ class MochiWarabi(CMakePackage):
     depends_on("fmt")
     depends_on("tclap", type=("build",))
     depends_on("valijson", when="@:0.2.0")
+    depends_on("py-pybind11", then="@0.7.0: +python")
 
     depends_on("mochi-thallium@0.11.3:")
     depends_on("mochi-bedrock@:0.14.2", when="+bedrock @:0.3.0")
@@ -55,6 +57,7 @@ class MochiWarabi(CMakePackage):
     depends_on("mochi-abt-io")
     depends_on("mochi-abt-io@0.8.0:", when="@0.6.0:")
     depends_on("mochi-remi", when="+remi")
+    depends_on("py-mochi-margo@0.6.0:", when="+python @0.7.0:")
 
     depends_on("mochi-thallium@0.12.0:", when="@0.2.0:")
     depends_on("mochi-margo@0.15.0:", when="@0.2.0:")
@@ -65,6 +68,7 @@ class MochiWarabi(CMakePackage):
     depends_on("mochi-bedrock-module-api@develop", when="@develop +bedrock")
     depends_on("mochi-abt-io@develop", when="@develop")
     depends_on("mochi-remi@develop", when="@develop +remi")
+    depends_on("py-mochi-margo@develop", when="+python @develop")
 
     depends_on("python", when="+python")
     depends_on("py-configspace@1.1.4:", when="+space")
