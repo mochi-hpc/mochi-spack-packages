b55a4de0b27d675e5dcb22cab5fbe16b18a963c8
Matthieu Dorier <mdorier@anl.gov>
2026-01-20 10:15:51 +0000

    added margo version 0.22.0

--- packages/mochi-margo/package.py
+++ packages/mochi-margo/package.py
@@ -31,6 +31,7 @@ class MochiMargo(cmake.CMakePackage, autotools.AutotoolsPackage):
     # upstream spack package as well:
     # https://github.com/spack/spack/tree/develop/var/spack/repos/builtin/packages/mochi-margo
     version('develop', branch='main')
+    version("0.22.0", sha256="65d9170e517779beea7ce6f251271602bbee98cc434312336d0dcc8496ffed58")
     version("0.21.0", sha256="d0a527cd0dcbeb9a8f04d090140cdedb66d9a90c6794a046d48d6bc2d11fc278")
     version("0.20.0", sha256="ed19f65c3c0dda42b285904f64508d1997f4b0fcef81cddb011aa9c42381eb2a")
     version("0.19.2", sha256="cfd20117744631779f0e99a0bc0668a1ca4d6d3c89fce5e9926961f830491689")
@@ -86,7 +87,8 @@ class MochiMargo(cmake.CMakePackage, autotools.AutotoolsPackage):
     version('0.7.2-exp', git='https://github.com/srini009/margo.git', branch='experimental')
 
     variant('pvar', default=False, description="extract performance data from Mercury")
-    variant('hwloc', default=True, when="@0.21:", description="use hwloc to help select network cards when possible")
+    variant('hwloc', default=True, when="@0.21:",
+            description="use hwloc to help select network cards when possible")
 
     depends_on("c", type="build")
     depends_on("cxx", type="build")
@@ -111,9 +113,14 @@ class MochiMargo(cmake.CMakePackage, autotools.AutotoolsPackage):
     depends_on('mercury@2.0.0:', type=("build", "link", "run"), when='@0.5.2:')
     depends_on('mercury@2.0.0rc1-pvar', when='@0.7.2-exp')
     depends_on('hwloc', when='+hwloc')
+    depends_on('libfabric', when='+hwloc')
 
     # dependencies for develop version
     depends_on('mercury@master', type=("build", "link", "run"), when='@develop')
+
+    def cmake_args(self):
+        args = [self.define_from_variant("ENABLE_PLUMBER", "hwloc")]
+        return args
 
     def autoreconf(self, spec, prefix):
         sh = which('sh')
