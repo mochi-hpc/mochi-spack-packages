d5f6cff45a9513badc67443a3cbcadded476acba
Matthieu Dorier <mdorier@anl.gov>
2026-03-31 14:26:20 +0100

    new version of thallium

--- packages/mochi-thallium/package.py
+++ packages/mochi-thallium/package.py
@@ -12,6 +12,7 @@ class MochiThallium(CMakePackage):
 
     version('main', branch='main')
     version('develop', branch='main')
+    version("0.15.2", sha256="15b8e1619ab66b55e4a1e16d126e37e3103b62580244b370edcad66992ffd14c")
     version("0.15.1", sha256="aca135efd5f870d7f1ba34a869239f9256efaa776b4706a45d2c28ca9ca27951")
     version("0.15.0", sha256="a7872e926e97fdf80a67c8e44f1217a959c689763dbcf9712abd913d1ef23bdf")
     version("0.14.6", sha256="dfdd39fc840a82a69c1698e764239e8aa0a5573f677a52fb2bdd6bffd529a232")
